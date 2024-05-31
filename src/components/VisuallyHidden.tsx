91ad424bc699f75636ce2955fbc13bc82fc58a5e
Luis Llanes <luisllaboj@gmail.com>
2024-05-31 01:40:53 -0700

    add visually hidden component for accesibility

---
+++ src/components/VisuallyHidden.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+
+interface VisuallyHiddenProps extends React.HTMLAttributes<HTMLSpanElement> {}
+
+export default function VisuallyHidden({
+  children,
+  ...props
+}: VisuallyHiddenProps) {
+  const [forceShow, setForceShow] = React.useState(false);
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV !== "production") {
+      const handleKeyDown = (ev: KeyboardEvent) => {
+        if (ev.key === "Alt") {
+          setForceShow(true);
+        }
+      };
+      const handleKeyUp = (ev: KeyboardEvent) => {
+        if (ev.key === "Alt") {
+          setForceShow(false);
+        }
+      };
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("keyup", handleKeyUp);
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keyup", handleKeyUp);
+      };
+    }
+  }, []);
+
+  if (forceShow) {
+    return <span>{children}</span>;
+  }
+
+  return (
+    <span className="sr-only" {...props}>
+      {children}
+    </span>
+  );
+}
