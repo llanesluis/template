cef01baa902ece027cfa55963980229e3a8d4f7e
Luis Llanes <luisllaboj@gmail.com>
2025-02-25 19:39:49 -0700

    remove duplicate class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={cn(
           geist.variable,
           geistMono.variable,
-          "animate-blurred-fade-in flex min-h-svh flex-col font-mono antialiased",
+          "animate-blurred-fade-in flex min-h-svh flex-col font-mono",
         )}
       >
         <Providers>
